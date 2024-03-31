544ebb6279e41c00a967f9e3c1412fc93fc7d8e7
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-03-31 21:49:49 +0300

    implement form component

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import { MatDivider } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatDivider,
+    MatButtonModule
+  ],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
