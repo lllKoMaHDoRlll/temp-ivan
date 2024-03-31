2866b19a44a5bb6dbb9dd10bbcda34dbbdbeb39b
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-03-31 18:08:54 +0300

    implement header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { HeaderModule } from '@coreui/angular';
+import { ContainerComponent } from '@coreui/angular';
+import { NavModule } from '@coreui/angular';
+import { DropdownModule } from '@coreui/angular';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderModule,
+    ContainerComponent,
+    NavModule,
+    DropdownModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
