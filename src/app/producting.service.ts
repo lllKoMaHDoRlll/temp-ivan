0d733e9832b974cce3d61c6789451846308af598
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2025-04-19 20:34:34 +0300

    change products price

--- src/app/producting.service.ts
+++ src/app/producting.service.ts
@@ -11,105 +11,105 @@ export class ProductingService {
       id: 1,
       name: "Adidas Superstar",
       imageFilePath: `${this.imageBaseFilePath}/1.jpg`,
-      price: 100,
+      price: 10000,
       description: ""
     },
     {
       id: 2,
       name: "Onitsuka Tiger",
       imageFilePath: `${this.imageBaseFilePath}/2.jpg`,
-      price: 70,
+      price: 7000,
       description: ""
     },
     {
       id: 3,
       name: "Adidas Smith",
       imageFilePath: `${this.imageBaseFilePath}/3.jpg`,
-      price: 120,
+      price: 12000,
       description: ""
     },
     {
       id: 4,
       name: "Converse Pro",
       imageFilePath: `${this.imageBaseFilePath}/4.jpg`,
-      price: 200,
+      price: 20000,
       description: ""
     },
     {
       id: 5,
       name: "Vans SK8 Hi",
       imageFilePath: `${this.imageBaseFilePath}/5.jpg`,
-      price: 175,
+      price: 17500,
       description: ""
     },
     {
       id: 6,
       name: "Nike Lava Dome",
       imageFilePath: `${this.imageBaseFilePath}/6.jpg`,
-      price: 130,
+      price: 13000,
       description: ""
     },
     {
       id: 7,
       name: "Adidas Forum",
       imageFilePath: `${this.imageBaseFilePath}/7.jpg`,
-      price: 220,
+      price: 22000,
       description: ""
     },
     {
       id: 8,
       name: "NB 1300",
       imageFilePath: `${this.imageBaseFilePath}/8.jpg`,
-      price: 200,
+      price: 20000,
       description: ""
     },
     {
       id: 9,
       name: "Nike Air Jordan",
       imageFilePath: `${this.imageBaseFilePath}/9.jpg`,
-      price: 300,
+      price: 30000,
       description: ""
     },
     {
       id: 10,
       name: "Adidas Campus",
       imageFilePath: `${this.imageBaseFilePath}/10.jpg`,
-      price: 110,
+      price: 11000,
       description: ""
     },
     {
       id: 11,
       name: "Diadora Maverick",
       imageFilePath: `${this.imageBaseFilePath}/11.jpg`,
-      price: 150,
+      price: 15000,
       description: ""
     },
     {
       id: 12,
       name: "Nike Air Max 1",
       imageFilePath: `${this.imageBaseFilePath}/12.jpg`,
-      price: 140,
+      price: 14000,
       description: ""
     },
     {
       id: 13,
       name: "Nike Air Jordan IV",
       imageFilePath: `${this.imageBaseFilePath}/13.jpg`,
-      price: 230,
+      price: 23000,
       description: ""
     },
     {
       id: 14,
       name: "Reebok The Pump",
       imageFilePath: `${this.imageBaseFilePath}/14.jpg`,
-      price: 370,
+      price: 37000,
       description: ""
     },
     {
       id: 15,
       name: "Nike Air Jordan V",
       imageFilePath: `${this.imageBaseFilePath}/15.jpg`,
-      price: 200,
+      price: 20000,
       description: ""
     }
   ];
